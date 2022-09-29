3156a7196398e506c913d1993da7372c174eedc9
ALINE FERNANDES AMARO <111320393+Aline-fas@users.noreply.github.com>
2022-09-28 21:30:59 -0300

    Exercicios em php

---
+++ aula01/ola.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    $linguagem = "PHP, HTML e CSS";
+    
+
+    // echo"<h1>Olá mundo. Esse é o meu primeiro programa em " .$linguagem. " </h1> ";
+    
+    // echo"Olá mundo. Esse é o meu primeiro programa em $linguagem "; 
+    
+    // printf("Olá mundo. Esse é o meu primeiro programa em  $linguagem ")
+
+    ?>
+</body>
+</html>
---
+++ aula01/prog04.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php
+        $num1 = $_GET["num1"];
+        $num2 = $_GET["num2"];
+    $num1 = 5;
+    $num2 = 8;
+    $soma = $var0 + $var1;
+
+
+    echo " o primeiro numero é  $var0"; 
+    echo " <br> meu seg n é  $var1 ";
+    echo " <br> a soma de " .$var0. " + " .$var1. " = " .$soma;
+
+        ?>
+    </div>
+</body>
+</html>
