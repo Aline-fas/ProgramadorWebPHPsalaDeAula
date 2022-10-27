936265469f86ad1166693a3902ed1aba6181ab23
ALINE FERNANDES AMARO <alinefas2020@gmail.com>
2022-10-27 13:47:23 -0300

    Merge branch 'master' of https://github.com/Aline-fas/ProgramadorWebPHPsalaDeAula

--- desafios/desafio_cpf_funcoes.php
+++ desafios/desafio_cpf_funcoes.php
@@ -2,8 +2,10 @@
 
 //desafio implementar a validação do CPF
 function verifica_cpf($cpf){
+
     $cpf = str_replace(".","",$cpf);
     $cpf = str_replace("-","",$cpf);
     $cpf_v = str_split($cpf);
-    return $cpf;
+    return $true;
 }
+
