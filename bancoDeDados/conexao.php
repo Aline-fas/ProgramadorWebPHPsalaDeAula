5b3e8919913e4e1382573735e0d8f267ca7a23d2
ALINE FERNANDES AMARO <111320393+Aline-fas@users.noreply.github.com>
2022-10-31 18:54:15 -0300

    copy

--- bancoDeDados/conexao.php
+++ bancoDeDados/conexao.php
@@ -1,15 +1,13 @@
 <?php
 
 $hostname = "localhost";
-$banco_de_dados = "banco_php";
+$banco_de_dados = "banco01";
 $usuario = "root";
 $senha = "";
 
-// mysqli(hostname, usuario, senha, banco);
-
-$conexao = new mysqli($hostname, $usuario , $senha, $banco_de_dados);
-if($conexao->connect_errno){
-    echo "Falha ao conectar:(". $conexao->connect_errno . ")". $conexao->connect_error;
+$conexao = new mysqli($hostname, $usuario, $senha, $banco_de_dados);
+if($conexao->connect_errno){ // error + number = errno
+    echo "Falha ao conectar: (" . $conexao->connect_errno .")" . $conexao->connect_error;
 } else {
     // echo "Conectado ao Banco.<br>";
 }
--- bancoDeDados/login.php
+++ bancoDeDados/login.php
@@ -1,7 +1,8 @@
 <?php
-    if(!isset($_SESSION)){
-        session_start();
-    }
+if(!isset($_SESSION)){
+    session_start();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -14,22 +15,23 @@
         integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilo.css" />
     <title>Senac - Curso de PHP</title>
-    
 </head>
 
 <body>
-    <div> 
-       <?php
-        $idUsuario = $_SESSION['id'];
-        $nomeUsuario = $_SESSION['nome'];
+    <div>
+    <?php 
+    $idUsuario = $_SESSION["id"];
+    $nomeUsuario = $_SESSION["nome"];
 
-        echo "<br><h2 style='text-align:center;'>Bem vindo ao sistema!<br>$nomeUsuario</h2>";
-       
-       ?>
-       <a href="index.php">
-        <i class="bi bi-reply" style="font-size: 2rem; color: cornflowerblue;"></i> 
+    echo "<br><h2 style='text-align: center;'>Bem vindo ao Sistema!<br>$nomeUsuario</h2>";
+
+    ?>
+    <a href="index.php" class="btn btn-link">
+            <i class="bi bi-reply" style="font-size: 2rem;"></i>
+
         </a>
     </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
         crossorigin="anonymous"></script>
