c613313e3f38fc80bf49d259471b853c96bf2fc9
ALINE FERNANDES AMARO <alinefas2020@gmail.com>
2022-11-30 14:13:54 -0300

    conexao hospedagem

--- projeto/src/conexao.php
+++ projeto/src/conexao.php
@@ -1,12 +1,12 @@
 <?php
 
 $hostname = "localhost";
-$banco_de_dados = "projeto";
-$usuario = "root";
-$senha = "";
-// $banco_de_dados = "id19904056_projetobd";
-// $usuario = "id19904056_desenvolvedor";
-// $senha = "b!!(=S-l1ylO/h*~";
+// $banco_de_dados = "projeto";
+// $usuario = "root";
+// $senha = "";
+$banco_de_dados = "id19904056_projetobd";
+$usuario = "id19904056_desenvolvedor";
+$senha = "b!!(=S-l1ylO/h*~";
 
 $conexao = new mysqli($hostname, $usuario, $senha, $banco_de_dados);
 if($conexao->connect_errno){ // error + number = errno
